a66cbfe937a4ea12f67ebb422490e162f34ff384
Raymond Mawunyo Aheto <40844791+mawunyoaheto@users.noreply.github.com>
2022-06-13 00:23:05 -0500

    second commit

--- src/main/java/com/raymond/springbootdockerjenkinsintegration/SpringbootDockerJenkinsIntegrationApplication.java
+++ src/main/java/com/raymond/springbootdockerjenkinsintegration/SpringbootDockerJenkinsIntegrationApplication.java
@@ -2,10 +2,17 @@ package com.raymond.springbootdockerjenkinsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDockerJenkinsIntegrationApplication {
 
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Jenkins Docker Integration demo!!!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDockerJenkinsIntegrationApplication.class, args);
     }
