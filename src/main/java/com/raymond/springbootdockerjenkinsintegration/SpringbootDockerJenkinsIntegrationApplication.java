81149602619d572ef280f3c92523da182c983aed
Raymond Mawunyo Aheto <40844791+mawunyoaheto@users.noreply.github.com>
2022-06-12 23:59:19 -0500

    initial commit

---
+++ src/main/java/com/raymond/springbootdockerjenkinsintegration/SpringbootDockerJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.raymond.springbootdockerjenkinsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerJenkinsIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerJenkinsIntegrationApplication.class, args);
+    }
+
+}
